bac9080151ebc17cfa17ab173be41249558175c7
Swagrzyk <mswagrzyk@interia.pl>
2023-03-31 22:58:37 +0200

    naprawa ekranu poczatkowego + poziom poprawa poziomu (zwiekszenie ilosci przeszkod) oraz naprawienie by dobrze je startowalo

--- giera/src/gra.java
+++ giera/src/gra.java
@@ -1,23 +1,29 @@
-import javax.swing.*;//
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;//do myszy
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;//keylistener-z klawiatury
+import java.awt.event.KeyListener;
 import java.util.Random;
-import java.util.logging.XMLFormatter;
 
 public class gra extends JPanel implements KeyListener, ActionListener {
-    private int x=300, y=875,wh=75;//ustawienia gracza
+    private int x=300;
+    private int y=875;
+    private final int wh=75;//ustawienia gracza
     private int speed;
     private int speedwrog;
-    private Timer time;//odswierzanie
+    private final Timer time;//odswierzanie
     private int ey=0;
-    private Random liczba =new Random();
+    public  int wybor;
+    private final Random liczba =new Random();
     private int wrogx1=liczba.nextInt(12)*100;//6 bo rozmiar okna to 600
     private int wrogx2=liczba.nextInt(12)*100;//6 bo rozmiar okna to 600
     private int wrogx3=liczba.nextInt(12)*100;//6 bo rozmiar okna to 600
-    private int wWroga=100, szWroga=100;
+    private int wrogx4=liczba.nextInt(12)*100;//6 bo rozmiar okna to 600
+    private int wrogx5=liczba.nextInt(12)*100;//6 bo rozmiar okna to 600
+    private int wrogx6=liczba.nextInt(12)*100;//6 bo rozmiar okna to 600
+    private final int wWroga=100;
+    private final int szWroga=100;
     private float punkty;
 
 
@@ -32,15 +38,21 @@ public class gra extends JPanel implements KeyListener, ActionListener {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
 
-        JPanel panel = new JPanel(new GridLayout(4, 1));
+        JPanel panel = new JPanel(new GridLayout(5, 1));
+        JLabel label = new JLabel("Wybierz poziom trudności " );
+        JLabel labelv1 = new JLabel(" " );
+        JLabel labelv2 = new JLabel("                                 Sterowanie strzałkami zwieksza" );
+        JLabel labelv3 = new JLabel("predkosc poruszania sie X2 " );
 
-        JLabel label = new JLabel("Wybierz poziom trudności");
         label.setHorizontalAlignment(JLabel.CENTER);
         panel.add(label);
+        panel.add(labelv1);
+
 
         JButton easyButton = new JButton("łatwy");
         easyButton.addActionListener(e -> {
-            speedwrog = 5;
+            wybor=1;
+            speedwrog = 10;
             speed=20;
             reset();
             frame.dispose();
@@ -49,7 +61,8 @@ public class gra extends JPanel implements KeyListener, ActionListener {
 
         JButton mediumButton = new JButton("średni");
         mediumButton.addActionListener(e -> {
-            speedwrog = 10;
+            wybor=2;
+            speedwrog = 14;
             speed=25;
             frame.dispose();
             reset();
@@ -58,7 +71,8 @@ public class gra extends JPanel implements KeyListener, ActionListener {
 
         JButton hardButton = new JButton("trudny");
         hardButton.addActionListener(e -> {
-            speedwrog = 20;
+            wybor=3;
+            speedwrog = 18;
             speed=50;
             frame.dispose();
             reset();
@@ -68,7 +82,8 @@ public class gra extends JPanel implements KeyListener, ActionListener {
         JButton exitButton = new JButton("Wyjdź");
         exitButton.addActionListener(e -> System.exit(0));
         panel.add(exitButton);
-
+        panel.add(labelv2);
+        panel.add(labelv3);
         frame.add(panel);
         frame.setVisible(true);
 
@@ -97,13 +112,32 @@ public class gra extends JPanel implements KeyListener, ActionListener {
         g.fillRect(x,y,wh,wh);
         //wrog
         g.setColor(Color.red);
-        g.fillRect(wrogx1,ey,wWroga,szWroga);
-        g.fillRect(wrogx2,ey,100,100);
-        g.fillRect(wrogx3,ey,100,100);
+        if(wybor==1) {
+            g.setColor(Color.red);
+            g.fillRect(wrogx1, ey, wWroga, szWroga);
+            g.fillRect(wrogx2, ey, 100, 100);
+            g.fillRect(wrogx3, ey, 100, 100);
+        }else if (wybor==2){
+            g.setColor(Color.red);
+            g.fillRect(wrogx1, ey, wWroga, szWroga);
+            g.fillRect(wrogx2, ey, 100, 100);
+            g.fillRect(wrogx3, ey, 100, 100);
+            g.fillRect(wrogx4, ey, 100, 100);
+
+        }else if (wybor==3){
+            g.setColor(Color.red);
+            g.fillRect(wrogx1, ey, wWroga, szWroga);
+            g.fillRect(wrogx2, ey, 100, 100);
+            g.fillRect(wrogx3, ey, 100, 100);
+            g.fillRect(wrogx4, ey, 100, 100);
+            g.fillRect(wrogx5, ey, 100, 100);
+            g.fillRect(wrogx6, ey, 100, 100);
+
+        }
         //punkty
         g.setColor(Color.yellow);
         setFont(new Font("serif",Font.BOLD,20));
-        g.drawString("punkty"+punkty,600,20);
+        g.drawString("punkty"+punkty,580,20);
     }
     @Override
 
@@ -111,36 +145,99 @@ public class gra extends JPanel implements KeyListener, ActionListener {
         repaint();//ciagle odswierza sie obraz
         ey += speedwrog;
         Rectangle gracz = new Rectangle(x,y,wh,wh);
-        Rectangle wrog1 = new Rectangle(wrogx1,ey,wWroga,szWroga);
-        Rectangle wrog2 = new Rectangle(wrogx2,ey,wWroga,szWroga);
-        Rectangle wrog3 = new Rectangle(wrogx3,ey,wWroga,szWroga);
-        if(gracz.intersects(wrog1)||gracz.intersects(wrog2)||gracz.intersects(wrog3)){
-            time.stop();
-            int choice = JOptionPane.showOptionDialog(this,
-                    "Game over! Co chcesz zrobić?",
-                    "Koniec gry",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    new Object[]{"Zagraj ponownie", "Wróć do menu wyboru poziomu trudności","Wyjdz z gry"},
-                    "Zagraj ponownie");
+        if(wybor==1) {
+            Rectangle wrog1 = new Rectangle(wrogx1,ey,wWroga,szWroga);
+            Rectangle wrog2 = new Rectangle(wrogx2,ey,wWroga,szWroga);
+            Rectangle wrog3 = new Rectangle(wrogx3,ey,wWroga,szWroga);
 
-            // wykonanie wybranego działania
-            if (choice == 0) { // zagraj ponownie
-                reset();
-            } else if (choice == 1) { // wróć do menu wyboru poziomu trudności
-                chooseDifficult();
-            }else if(choice ==2){
-                System.exit(0);
+            if(gracz.intersects(wrog1)||gracz.intersects(wrog2)||gracz.intersects(wrog3)){
+                time.stop();
+                int choice = JOptionPane.showOptionDialog(this,
+                        "Game over! Co chcesz zrobić?",
+                        "Koniec gry",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        new Object[]{"Zagraj ponownie", "Wróć do menu wyboru poziomu trudności","Wyjdz z gry"},
+                        "Zagraj ponownie");
+
+                // wykonanie wybranego działania
+                if (choice == 0) { // zagraj ponownie
+                    reset();
+                } else if (choice == 1) { // wróć do menu wyboru poziomu trudności
+                    chooseDifficult();
+                }else if(choice ==2){
+                    System.exit(0);
+                }
+
+            }
+        }else if(wybor==2) {
+            Rectangle wrog1 = new Rectangle(wrogx1,ey,wWroga,szWroga);
+            Rectangle wrog2 = new Rectangle(wrogx2,ey,wWroga,szWroga);
+            Rectangle wrog3 = new Rectangle(wrogx3,ey,wWroga,szWroga);
+            Rectangle wrog4 = new Rectangle(wrogx4,ey,wWroga,szWroga);
+
+            if(gracz.intersects(wrog1)||gracz.intersects(wrog2)||gracz.intersects(wrog3)||gracz.intersects(wrog4)){
+                time.stop();
+                int choice = JOptionPane.showOptionDialog(this,
+                        "Game over! Co chcesz zrobić?",
+                        "Koniec gry",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        new Object[]{"Zagraj ponownie", "Wróć do menu wyboru poziomu trudności","Wyjdz z gry"},
+                        "Zagraj ponownie");
+
+                // wykonanie wybranego działania
+                if (choice == 0) { // zagraj ponownie
+                    reset();
+                } else if (choice == 1) { // wróć do menu wyboru poziomu trudności
+                    chooseDifficult();
+                }else if(choice ==2){
+                    System.exit(0);
+                }
+
+            }
+        } else if (wybor==3) {
+            Rectangle wrog1 = new Rectangle(wrogx1,ey,wWroga,szWroga);
+            Rectangle wrog2 = new Rectangle(wrogx2,ey,wWroga,szWroga);
+            Rectangle wrog3 = new Rectangle(wrogx3,ey,wWroga,szWroga);
+            Rectangle wrog4 = new Rectangle(wrogx4,ey,wWroga,szWroga);
+            Rectangle wrog5 = new Rectangle(wrogx5,ey,wWroga,szWroga);
+            Rectangle wrog6 = new Rectangle(wrogx6,ey,wWroga,szWroga);
+            if(gracz.intersects(wrog1)||gracz.intersects(wrog2)||gracz.intersects(wrog3)||gracz.intersects(wrog4)||gracz.intersects(wrog5)||gracz.intersects(wrog6)){
+                time.stop();
+                int choice = JOptionPane.showOptionDialog(this,
+                        "Game over! Co chcesz zrobić?",
+                        "Koniec gry",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        new Object[]{"Zagraj ponownie", "Wróć do menu wyboru poziomu trudności","Wyjdz z gry"},
+                        "Zagraj ponownie");
+
+                // wykonanie wybranego działania
+                if (choice == 0) { // zagraj ponownie
+                    reset();
+                } else if (choice == 1) { // wróć do menu wyboru poziomu trudności
+                    chooseDifficult();
+                }else if(choice ==2){
+                    System.exit(0);
+                }
+
             }
 
         }
 
-         if (ey == 850) {
-            ey=30;
+
+        if (ey >= 850) {
+            ey=0;
             wrogx1 = liczba.nextInt(12) * 100;
             wrogx3 = liczba.nextInt(12) * 100;
             wrogx2 = liczba.nextInt(12) * 100;
+            wrogx4 = liczba.nextInt(12) * 100;
+            wrogx5 = liczba.nextInt(12) * 100;
+            wrogx6 = liczba.nextInt(12) * 100;
             punkty++;
         }
     }
@@ -168,6 +265,23 @@ public class gra extends JPanel implements KeyListener, ActionListener {
                 }
 
             }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (x == 0) {
+                x = 0;
+            } else {
+                x -= 2*speed;//porusza sie w lewo
+            }
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (x == 1100) {
+                x = 1100;
+            } else {
+                x += 2*speed;//porusza sie w prawo
+
+            }
+
+        }
 
         if(e.getKeyCode()==KeyEvent.VK_W){
             if (y ==15 ) {
@@ -184,6 +298,21 @@ public class gra extends JPanel implements KeyListener, ActionListener {
                     y += speed;//porusza sie do przodu
                 }
         }
+        if(e.getKeyCode()==KeyEvent.VK_UP){
+            if (y ==15 ) {
+                y = 15;
+            } else {
+                y -= 2*speed;//porusza sie do tylu
+            }
+        }
+        if(e.getKeyCode()==KeyEvent.VK_DOWN){
+            if (y ==875){
+                y=875;
+            }
+            else{
+                y += 2*speed;//porusza sie do przodu
+            }
+        }
             if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                 reset();
             }
@@ -194,6 +323,9 @@ public class gra extends JPanel implements KeyListener, ActionListener {
         wrogx1 =liczba.nextInt(12)*100;
         wrogx3 =liczba.nextInt(12)*100;
         wrogx2 =liczba.nextInt(12)*100;
+        wrogx4 =liczba.nextInt(12)*100;
+        wrogx5 =liczba.nextInt(12)*100;
+        wrogx6 =liczba.nextInt(12)*100;
         x=600;
         time.start();
     }
